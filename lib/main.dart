0fefb26bab9026973a81a844b392b6594901b22a
zlink <djyzzc@gmail.com>
2020-01-20 17:04:08 +0800

    flutter todolist init

---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<Todo> todos = [];
+
+  _toggleTodo(Todo todo, bool isChecked) {
+    setState(() {
+      todo.isDone = isChecked;
+    });
+  }
+
+  _addTodo() async {
+    final todo = await showDialog<Todo>(
+        context: context,
+        builder: (BuildContext context) {
+          return TodoDialog();
+        });
+
+    if (todo != null) {
+      setState(() {
+        todos.add(todo);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Todo List'),),
+      body: TodoList(
+        todos: todos,
+        onTodoToggle: _toggleTodo,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _addTodo,
+      ),
+    );
+  }
+}
+
+class Todo {
+  Todo({this.title, this.isDone = false});
+
+  String title;
+  bool isDone;
+}
+
+typedef ToggleTodoCallback = void Function(Todo, bool);
+
+class TodoList extends StatelessWidget {
+  const TodoList({@required this.todos, this.onTodoToggle});
+
+  final List<Todo> todos;
+
+  final ToggleTodoCallback onTodoToggle;
+
+  Widget _buildItem(BuildContext context, int index) {
+    final todo = todos[index];
+
+    return CheckboxListTile(
+        title: Text(todo.title),
+        value: todo.isDone,
+        onChanged: (bool isChecked) {
+          onTodoToggle(todo, isChecked);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: _buildItem,
+      itemCount: todos.length,
+    );
+  }
+}
+
+class TodoDialog extends StatelessWidget {
+  final controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('NEW TODO'),
+      content: TextField(
+        controller: controller,
+        autofocus: true,
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Cancel'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text('Add'),
+          onPressed: () {
+            final todo = new Todo(title: controller.value.text);
+            controller.clear();
+
+            Navigator.of(context).pop(todo);
+          },
+        )
+      ],
+    );
+  }
+}
